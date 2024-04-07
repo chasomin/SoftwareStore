b1eac695b0b196dec1ff18d2d544bf756d8379f0
chasomin <somin49@naver.com>
2024-04-08 01:01:17 +0900

    Feat: Cell download 버튼 showAlert 연결, 무료/유료 앱 버튼 필터링

--- SoftwareStore/Search/SearchViewController.swift
+++ SoftwareStore/Search/SearchViewController.swift
@@ -37,19 +37,58 @@ final class SearchViewController: UIViewController, CommonViewController {
     }
     
     private func bind() {
-        let input = SearchViewModel.Input(searchButtonTap: customNavigationView.searchBar.rx.searchButtonClicked, searchText: customNavigationView.searchBar.rx.text, tableViewItem: tableView.rx.modelSelected(Result.self))
+        
+        let input = SearchViewModel.Input(searchButtonTap: customNavigationView.searchBar.rx.searchButtonClicked, searchText: customNavigationView.searchBar.rx.text, tableViewItem: tableView.rx.modelSelected(Result.self), freeButtonTap: freeAppFilterButton.rx.tap, paidButtonTap: paidAppFilterButton.rx.tap)
         let output = viewModel.transform(input: input)
         output.software
             .bind(to: tableView.rx.items(cellIdentifier: SearchTableViewCell.id, cellType: SearchTableViewCell.self)) { row, element, cell in
                 cell.configureCell(element: element)
+                
+                cell.downloadButton.rx.tap
+                    .map { element.formattedPrice }
+                    .bind(with: self, onNext: { owner, value in
+                        owner.showAlert(text: value)
+                    })
+                    .disposed(by: cell.disposeBag)
+
             }
             .disposed(by: disposeBag)
+        
         output.selectSofeware
             .drive(with: self) { owner, result in
                 let vc = SoftwareDetailViewController(software: result)
                 owner.navigationController?.pushViewController(vc, animated: true)
             }
             .disposed(by: disposeBag)
+        
+        output.freeButtonTap
+            .drive(with: self) { owner, result in
+                owner.paidAppFilterButton.configuration = UIButton.unselectedButton()
+                owner.paidAppFilterButton.setTitle("유료 앱", for: .normal)
+                owner.freeAppFilterButton.configuration = UIButton.selectedButton()
+            }
+            .disposed(by: disposeBag)
+        
+        output.paidButtonTap
+            .drive(with: self) { owner, result in
+                owner.freeAppFilterButton.configuration = UIButton.unselectedButton()
+                owner.paidAppFilterButton.configuration = UIButton.selectedButton()
+                owner.freeAppFilterButton.setTitle("무료 앱", for: .normal)
+            }
+            .disposed(by: disposeBag)
+        
+
+
+        
+//        output.downloadTap
+//            .withLatestFrom(output.selectSofeware)
+//            .map { $0.price.description }
+//            .debug()
+//            .drive(with: self, onNext: { owner, value in
+//                owner.showAlert(text: value)
+//            })
+//            .disposed(by: disposeBag)
+        
     }
 }
 
@@ -78,20 +117,42 @@ extension SearchViewController {
         hStack.axis = .horizontal
         hStack.spacing = 10
         hStack.distribution = .fillEqually
-        
-        var config = UIButton.Configuration.filled()
-        config.cornerStyle = .capsule
-        config.baseBackgroundColor = .black
-        config.baseForegroundColor = .systemMint
-        
+
         freeAppFilterButton.setTitle("무료 앱", for: .normal)
-        freeAppFilterButton.configuration = config
+        freeAppFilterButton.configuration = UIButton.selectedButton()
         paidAppFilterButton.setTitle("유료 앱", for: .normal)
-        paidAppFilterButton.configuration = config
+        paidAppFilterButton.configuration = UIButton.selectedButton()
 
         tableView.backgroundColor = .black
         tableView.layer.cornerRadius = 30
         tableView.rowHeight = 100
         tableView.register(SearchTableViewCell.self, forCellReuseIdentifier: SearchTableViewCell.id)
+    }
+}
+
+extension UIViewController {
+    func showAlert(text: String) {
+        let alert = UIAlertController(title: "가격", message: "이 상품의 가격은 \(text) 입니다!", preferredStyle: .alert)
+        let button = UIAlertAction(title: "확인", style: .default)
+        alert.addAction(button)
+        present(alert, animated: true)
+    }
+}
+
+
+extension UIButton {
+    static func selectedButton() -> UIButton.Configuration {
+        var config = UIButton.Configuration.filled()
+        config.cornerStyle = .capsule
+        config.baseBackgroundColor = .black
+        config.baseForegroundColor = .systemMint
+        return config
+    }
+    
+    static func unselectedButton() -> UIButton.Configuration {
+        var config = UIButton.Configuration.plain()
+        config.cornerStyle = .capsule
+        config.baseForegroundColor = .black
+        return config
     }
 }
--- SoftwareStore/Search/SearchViewModel.swift
+++ SoftwareStore/Search/SearchViewModel.swift
@@ -16,14 +16,21 @@ final class SearchViewModel: CommonViewModel {
         let searchButtonTap: ControlEvent<Void>
         let searchText: ControlProperty<String?>
         let tableViewItem: ControlEvent<Result>
+//        let downloadTap: PublishRelay<ControlEvent<Void>>
+        let freeButtonTap: ControlEvent<Void>
+        let paidButtonTap: ControlEvent<Void>
     }
     
     struct Output {
         let software: PublishSubject<[Result]>
         let selectSofeware: Driver<Result>
+//        let downloadTap: Driver<Void>
+        let freeButtonTap: Driver<[Result]>
+        let paidButtonTap: Driver<[Result]>
     }
     
     func transform(input: Input) -> Output {
+        let origin = PublishSubject<[Result]>()
         let software = PublishSubject<[Result]>()
 
         input.searchButtonTap
@@ -33,6 +40,7 @@ final class SearchViewModel: CommonViewModel {
             }
             .subscribe(onNext: { result in
                 software.onNext(result)
+                origin.onNext(result)
             }, onError: { _ in
                 print("==== Transform Error")
             }, onCompleted: {
@@ -42,7 +50,26 @@ final class SearchViewModel: CommonViewModel {
             })
             .disposed(by: disposeBag)
                 
-        return Output.init(software: software, selectSofeware: input.tableViewItem.asDriver())
+        let freeButtonTap = input.freeButtonTap
+            .withLatestFrom(origin)
+            .map { $0.filter { $0.formattedPrice == "무료" }}
+            .asDriver(onErrorJustReturn: [])
+        
+        freeButtonTap
+            .drive(software)
+            .disposed(by: disposeBag)
+        
+        let paidButtonTap = input.paidButtonTap
+            .withLatestFrom(origin)
+            .map { $0.filter { $0.formattedPrice != "무료" }}
+            .asDriver(onErrorJustReturn: [])
+        
+        paidButtonTap
+            .drive(software)
+            .disposed(by: disposeBag)
+
+        
+        return Output.init(software: software, selectSofeware: input.tableViewItem.asDriver(), freeButtonTap: freeButtonTap, paidButtonTap: paidButtonTap)
     }
     
 }
