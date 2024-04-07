86282f9f9e5b17f058e855a48d38a32228136e30
chasomin <somin49@naver.com>
2024-04-07 23:31:32 +0900

    Feat: Network Observable 리턴

--- SoftwareStore/Network/StoreNetwork.swift
+++ SoftwareStore/Network/StoreNetwork.swift
@@ -6,32 +6,49 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+enum APIError: String, Error {
+    case invalidURL = "잘못된 URL"
+    case unknownResponse = "알 수 없는 응답"
+    case statusError = "상태코드 오류"
+    case decodingError = "응답은 왔으나 디코딩 실패"
+}
 
 
 final class StoreNetwork {
-    static func fetchStore(searchText: String) {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=\(searchText)&country=KR&media=software") else {
-            return
+    static func fetchStore(searchText: String) -> Observable<[Result]> {
+        
+        return Observable<[Result]>.create { observer in
+            guard let url = URL(string: "https://itunes.apple.com/search?term=\(searchText)&country=KR&media=software") else {
+                observer.onError(APIError.invalidURL)
+                return Disposables.create()
+            }
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                if let _ = error {
+                    observer.onError(APIError.unknownResponse)
+                    return
+                }
+                
+                guard let response = response as? HTTPURLResponse, (200...299).contains(response.statusCode) else {
+                    observer.onError(APIError.statusError)
+                    return
+                }
+                
+                guard let data else { return }
+                
+                do {
+                    let appData = try JSONDecoder().decode(Store.self, from: data)
+                    observer.onNext(appData.results)
+                    observer.onCompleted() //✅
+                }
+                catch {
+                    observer.onError(APIError.decodingError)
+                }
+            }
+            .resume()
+            return Disposables.create()
         }
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            if let _ = error {
-                return
-            }
-            
-            guard let response = response as? HTTPURLResponse, (200...299).contains(response.statusCode) else {
-                return
-            }
-            
-            guard let data else { return }
-            
-            do {
-                let appData = try JSONDecoder().decode(Store.self, from: data)
-                print(appData)
-            }
-            catch {
-                print("디코딩 실패")
-            }
-        }
-        .resume()
     }
 }
